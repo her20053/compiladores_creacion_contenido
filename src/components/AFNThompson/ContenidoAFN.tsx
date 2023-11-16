927d910792f8f94b2dd2669c013b382ab465c5ce
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-11-16 08:04:15 -0600

    fetching al sitio correcto

--- src/components/AFNThompson/ContenidoAFN.tsx
+++ src/components/AFNThompson/ContenidoAFN.tsx
@@ -1,47 +1,54 @@
 // Importa las dependencias necesarias de React y cualquier otra que necesites
 import React, { useState, FormEvent } from 'react';
 
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { TextInput, Button, Group, Box, Center } from '@mantine/core';
 import { useForm } from '@mantine/form';
 
-import { Center } from '@mantine/core';
-
 import { Image } from '@mantine/core';
+
 
 import ImagenPlaceHolder from '../../assets/placeholder-image.png';
 
 // Define tu componente como una función de flecha
 export const AFNThompsonComponent: React.FC = () => {
-
     const form = useForm({
         initialValues: {
             regex: '',
         },
 
         validate: {
-            regex: (value) => (value.length < 20 ? null : 'Porfavor utiliza menos de 20 caracteres.'),
+            regex: (value) => (value.length < 20 ? null : 'Por favor utiliza menos de 20 caracteres.'),
         },
     });
 
-    // Estado para la URL de la imagen
     const [imageUrl, setImageUrl] = useState<string>(ImagenPlaceHolder);
 
-    // Función para manejar el envío del formulario
-    const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Previene el comportamiento predeterminado del formulario
+    const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         try {
-            // La URL de la API que devuelve un GIF (Por ejemplo: una API pública de GIFs)
-            const gifApiUrl = 'https://avatars.githubusercontent.com/u/70614527?v=4';
+            const formData = new FormData();
+            formData.append('user_input', form.values.regex);
 
-            const response = await fetch(gifApiUrl);
-            const data = await response.json();
+            const response = await fetch('http://3.224.178.178:5000/generate', {
+                method: 'POST',
+                body: formData,
+            });
 
-            // Asumiendo que la respuesta tiene una propiedad `data` que es la URL del GIF
-            setImageUrl(data.data);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const blob = await response.blob();
+            const url = URL.createObjectURL(blob);
+            setImageUrl(url);
         } catch (error) {
             console.error("Error fetching the GIF:", error);
+            // Mostrar una alerta emergente de error con window.alert()
+            window.alert("Hubo un error al generar el GIF. Por favor, verifica la expresión regular.");
+            setImageUrl(ImagenPlaceHolder); // Vuelve a la imagen de placeholder en caso de error
         }
     };
+
 
     return (
         <Center h={1000} bg="var(--mantine-color-gray-light)">
