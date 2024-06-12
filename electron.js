1e18b26527cf762cc955998d178e92831e700e3b
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2024-06-12 13:27:03 -0600

    Fixing electron connection

--- electron.js
+++ electron.js
@@ -1,30 +1,42 @@
 const { app, BrowserWindow } = require('electron');
-const { spawn } = require('child_process');
-
-let flaskProcess = null;
+const path = require('path');
+const spawn = require('child_process').spawn;
 
 function createWindow() {
-
-    flaskProcess = spawn('python', ['./backend/main.py']);
-
-    flaskProcess.stdout.on('data', (data) => {
-      console.log(`Flask: ${data}`);
+    const python = spawn('gunicorn', ['-w', '4', '-b', '127.0.0.1:5000', 'main:app'], {
+        cwd: path.join(__dirname, 'backend'),
+        env: {
+            ...process.env,
+            PYTHONPATH: process.env.PYTHONPATH
+                ? `${process.env.PYTHONPATH}:${path.join(__dirname, 'backend')}`
+                : path.join(__dirname, 'backend')
+        }
     });
-  
-    const mainWindow = new BrowserWindow({
+
+    python.stdout.on('data', function (data) {
+        console.log("data: ", data.toString('utf8'));
+    });
+
+    python.stderr.on('data', function (data) {
+        console.log(`stderr: ${data.toString('utf8')}`);
+    });
+
+    const win = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences: {
-        nodeIntegration: true,
-        },
-  });
+    });
 
-  mainWindow.loadURL('http://127.0.0.1:5173');
-  mainWindow.webContents.openDevTools();
+    win.loadURL('http://127.0.0.1:5173');
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+    createWindow();
 
-app.on('will-quit', () => {
-    flaskProcess.kill();
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
